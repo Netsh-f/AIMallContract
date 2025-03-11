7690958b72882afbb7e311efc94c3453925f9f0d
Netsh-f <zwj1813@163.com>
2025-03-11 10:30:05 +0800

    初步创建，ERC20测试中

--- AIMallContract.sol
+++ AIMallContract.sol
@@ -1,2 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+import "./Struct.sol";
+import "./Error.sol";
+
+contract AIMallContract {
+    address private immutable owner;
+    IERC20 private immutable usdtToken;
+
+    modifier onlyOwner() {
+        if (msg.sender != owner) {
+            revert AIMallNotOwner();
+        }
+        _;
+    }
+
+    constructor(address usdtTokenAddress) {
+        owner = msg.sender;
+        usdtToken = IERC20(usdtTokenAddress);
+    }
+
+    mapping (uint256 => Model) private id_model;
+
+    mapping (address => uint256[]) private userAddress_modelIds;
+
+    function deposit(uint256 amount) public {
+        bool success = usdtToken.transferFrom(msg.sender, address(this), amount);
+        if (!success) {
+            revert TransferFailed();
+        }
+    }
+}
---
+++ Struct.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+struct Model {
+    string name;
+    string imageUrl;
+    uint256 price;
+    address owner;
+}
